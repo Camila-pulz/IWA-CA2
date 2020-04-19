3f35b3eba3d4cbd601abca09f83305b952c3fa94
Camila-pulz <camilapfaria@hotmail.com>
2020-04-19 10:40:06 +0000

    connection to the database

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+//const MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://camilaf:sn2017162@clusterca2-0oks7.mongodb.net/test?retryWrites=true&w=majority";
+//const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
 
 //Routes
 app.get('/', (req,res) =>{
     res.send('We are on the home');
 
 });
+
+app.get('/posts', (req, res) =>{
+    res.send("We are on post");
+});
+
+//connect to the database 
+mongoose.connect(
+    'mongodb+srv://camilaf:sn2017162@clusterca2-0oks7.mongodb.net/test?retryWrites=true&w=majority',
+   { useNewUrlParser: true },
+    () => console.log('Connected to the database')
+);
+
+
+
+
+
+
+//client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+ // client.close();
+//});
+
 
 
 //listening to the server
