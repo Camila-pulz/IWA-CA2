0a1fdf2675597ccd0816308f6db5495ea923018b
Camila-pulz <camilapfaria@hotmail.com>
2020-04-19 13:56:29 +0000

    retrieve specific record or all

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ mongoose.connect(
 );
 
 
-
-
-
-
 //client.connect(err => {
   //const collection = client.db("test").collection("devices");
   // perform actions on the collection object
--- routes.js
+++ routes.js
@@ -2,22 +2,36 @@ const express = require('express');
 const router = express.Router();
 const Media = require("./model/media"); //to use the schema from the media file
 
+//to get all the records on the database 
 router.get('/', (req,res) =>{
-    res.send('We are on the home');
-
+    Media.find({}, function (err, media) {
+    if (err) {
+      res.status(400).json(err); 
+    } 
+    res.json(media);
+  }); 
 });
 
+//to get a specific media from the database 
+router.get('/:id', (req,res) =>{
+    Media.findOne({_id: req.params.id}, function (err, media) {
+    if (err) {
+      res.status(400).json(err);
+    } 
+    res.json(media);
+  }); 
+});
+
+
+//To save a new media to the database 
 router.post('/', (req,res) => {
-    const newmedia = new Media(req.body);
-    newmedia.save(function (err, media) { //save the media to the MongoDB
-        if (err) { 
+    const newmedia = new Media(req.body);//creation of a new media based on the data which was inserted in postman
+    newmedia.save(function (err, media) { //function that saves the data into MongoDB
+        if (err) { //throws an error if there is any issue
             res.status(400).json(err);
         }
-
-        res.json(media); 
+        res.json(media); //send the new created media
 });
 });
-
-   
 
 module.exports = router;
